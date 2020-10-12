9f936a3ed0a277bb1eb4885c729e5b6ab97b0b5a
krouspy <knjlau1@gmail.com>
2020-10-12 20:50:42 +0200

    ERC721: use mappings instead of array to check if is tenant

--- contracts/ERC721/Landlord.sol
+++ contracts/ERC721/Landlord.sol
@@ -7,6 +7,9 @@ contract Landlord is LeaseFactory {
     event TenantRegistered(uint256 tokenId, address indexed tenant);
     event TenantRemoved(uint256 tokenId, address indexed tenant);
 
+    mapping(address => bool) public isTenant;
+    mapping(address => uint256) public tenantToTokenId;
+
     // solhint-disable-next-line no-empty-blocks
     constructor(string memory name, string memory symbol) public LeaseFactory(name, symbol) {}
 
@@ -21,7 +24,9 @@ contract Landlord is LeaseFactory {
         onlyLandlordOf(tokenId)
         returns (bool)
     {
-        require(!_isTenantOf(tokenId, account), "Landlord: address is already a tenant");
+        _checkAddressAndTokenId(account, tokenId);
+
+        require(!isTenant[account], "Landlord: address already tenant");
 
         Lease storage lease = leaseById[tokenId];
         uint256 maxTenants = lease.maxTenants;
@@ -30,6 +35,9 @@ contract Landlord is LeaseFactory {
         require(tenants.length < maxTenants, "Landlord: max tenants already reached");
 
         lease.tenants.push(account);
+
+        isTenant[account] = true;
+        tenantToTokenId[account] = tokenId;
 
         emit TenantRegistered(tokenId, account);
 
@@ -41,7 +49,9 @@ contract Landlord is LeaseFactory {
         onlyLandlordOf(tokenId)
         returns (bool)
     {
-        require(_isTenantOf(tokenId, account), "Landlord: address is not a tenant");
+        _checkAddressAndTokenId(account, tokenId);
+
+        require(isTenant[account], "Landlord: address not tenant");
 
         Lease storage lease = leaseById[tokenId];
         address[] storage tenants = lease.tenants;
@@ -60,25 +70,16 @@ contract Landlord is LeaseFactory {
             }
         }
 
+        isTenant[account] = false;
+        delete tenantToTokenId[account];
+
         emit TenantRemoved(tokenId, account);
 
         return true;
     }
 
-    function _isTenantOf(uint256 tokenId, address account) private view returns (bool) {
-        require(account != address(0), "Landlord: account is address(0)");
-        require(tokenId >= 0, "Landlord: token id is lower than 0");
-        require(tokenId < _tokenIds.current(), "Landlord: token id greater than total tokens");
-
-        Lease memory lease = leaseById[tokenId];
-        address[] memory tenants = lease.tenants;
-
-        for (uint256 i = 0; i < tenants.length; i++) {
-            if (tenants[i] == account) {
-                return true;
-            }
-        }
-
-        return false;
+    function _checkAddressAndTokenId(address account, uint256 tokenId) private view {
+        require(account != address(0), "Landlord: address is address(0)");
+        require(tokenId < _tokenIds.current(), "Landlord: tokenId too large");
     }
 }
