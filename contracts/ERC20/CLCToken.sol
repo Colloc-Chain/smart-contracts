1558d5d2b96257726f7614589fa3a1147597913a
krouspy <knjlau1@gmail.com>
2020-11-28 13:21:09 +0100

    add CreateERC20 event

--- contracts/ERC20/CLCToken.sol
+++ contracts/ERC20/CLCToken.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.6.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CLCToken is ERC20 {
+    event CreateERC20(address indexed contractAddress, string name, string symbol);
+
     // solhint-disable-next-line no-empty-blocks
-    constructor(string memory name, string memory symbol) public ERC20(name, symbol) {}
+    constructor(string memory name, string memory symbol) public ERC20(name, symbol) {
+        emit CreateERC20(address(this), name, symbol);
+    }
 
     function deposit(int256 amount) public returns (bool) {
         require(amount > 0, "ERC20: deposit zero or negative amount");
